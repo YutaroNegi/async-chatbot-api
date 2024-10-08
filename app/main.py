ccd99f02931549645738e4ba8faf3adbfc87bccc
Yutaro Negi <souza_yutaro@hotmail.com>
2024-10-08 15:32:29 -0300

    chore: add CORS middleware for handling cross-origin requests

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import logging
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app import config
+
 
 from app.routers import health, users, messages
 
@@ -20,6 +22,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[config.CORS_ALLOWED_DOMAIN],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(health.router)
 app.include_router(users.router)
