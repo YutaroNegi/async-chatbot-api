09a7fefdf820d04f915745709bee651c1e7e746c
Yutaro Negi <souza_yutaro@hotmail.com>
2024-10-09 06:51:05 -0300

    refactor: update authentication error message and status_code in test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -7,14 +7,11 @@ client = TestClient(app)
 
 def test_access_protected_route_without_token():
     response = client.post("/messages/", json={"message": "Hello"})
-    assert response.status_code == 403
+    assert response.status_code == 401
 
 
 def test_access_protected_route_with_invalid_token():
     headers = {"Authorization": "Bearer invalid_token"}
     response = client.post("/messages/", headers=headers, json={"message": "Hello"})
-    assert response.status_code == 403
-    assert (
-        response.json()["detail"]
-        == "Invalid token header. Use an appropriate JWT token."
-    )
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Not authenticated"
